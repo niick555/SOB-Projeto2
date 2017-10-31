f7da12ebca3215858c9bb10a58e9055f583cba79
IngridFrancoise11 <ingridfrancoise11@gmail.com>
2017-10-31 21:42:02 -0200

    Alteramos o sistema de leitura e escrita do minix para debug.

--- Makefile
+++ Makefile
@@ -1,23 +1,21 @@
-obj-m += cryptodevice.o
+#
+# Makefile for the Linux minix filesystem routines.
+#
 
+obj-$(CONFIG_MINIX_FS) += minix_module.o
+
+minix_module-objs := bitmap.o itree_v1.o itree_v2.o namei.o inode.o file.o dir.o
+
+KVERSION = $(shell name -r)
 all:
-	make cryptodevice
-	make programa
-
-cryptodevice:
 	make -C /lib/modules/$(shell uname -r)/build M=$(shell pwd) modules
-
-programa: programa.c
-	gcc programa.c -o programa
 
 clean:
 	make -C /lib/modules/$(shell uname -r)/build M=$(shell pwd) clean
 
 delete:
-	rm cryptodevice.ko
-	rm cryptodevice.mod.c
-	rm cryptodevice.mod.o
-	rm cryptodevice.o
-	rm Module.symvers
-	rm modules.order
-	rm programa
+	find . -name "*.ko" -type f -delete
+	find . -name "*.o" -type f -delete
+	find . -name "*.mod.c" -type f -delete
+	find . -name "*.symvers" -type f -delete
+	find . -name "*.order" -type f -delete
---
+++ file.c
@@ -0,0 +1,76 @@
+/*
+ *  linux/fs/minix/file.c
+ *
+ *  Copyright (C) 1991, 1992 Linus Torvalds
+ *
+ *  minix regular file handling primitives
+ */
+
+#include "minix.h"
+
+static ssize_t teste(struct kiocb *iocb, struct iov_iter *from);
+static ssize_t teste2(struct kiocb *iocb, struct iov_iter *iter);
+
+/*
+ * We have mostly NULLs here: the current defaults are OK for
+ * the minix filesystem.
+ */
+const struct file_operations minix_file_operations = {
+	.llseek		= generic_file_llseek,
+	.read_iter	= teste2,
+	.write_iter	= teste,
+	.mmap		= generic_file_mmap,
+	.fsync		= generic_file_fsync,
+	.splice_read	= generic_file_splice_read,
+};
+
+static ssize_t teste(struct kiocb *iocb, struct iov_iter *from)
+{
+	ssize_t ret;
+
+	ret = generic_file_write_iter(iocb, from);
+
+	printk("minix: teste_write %i\n", ret);
+
+	return ret;
+}
+
+static ssize_t teste2(struct kiocb *iocb, struct iov_iter *iter)
+{
+	ssize_t ret;
+
+	ret = generic_file_read_iter(iocb, iter);
+
+	printk("minix: teste_read %i\n", ret);
+
+	return ret;
+}
+
+static int minix_setattr(struct dentry *dentry, struct iattr *attr)
+{
+	struct inode *inode = d_inode(dentry);
+	int error;
+
+	error = inode_change_ok(inode, attr);
+	if (error)
+		return error;
+
+	if ((attr->ia_valid & ATTR_SIZE) &&
+	    attr->ia_size != i_size_read(inode)) {
+		error = inode_newsize_ok(inode, attr->ia_size);
+		if (error)
+			return error;
+
+		truncate_setsize(inode, attr->ia_size);
+		minix_truncate(inode);
+	}
+
+	setattr_copy(inode, attr);
+	mark_inode_dirty(inode);
+	return 0;
+}
+
+const struct inode_operations minix_file_inode_operations = {
+	.setattr	= minix_setattr,
+	.getattr	= minix_getattr,
+};
