aac6be70726ea71185bc1ecf25b831d5249e27ec
NICOLAS ALVES DE SOUZA FERREIRA DA SILVA <nicolas.asfs@puccampinas.edu.br>
2017-11-18 15:59:27 -0200

    VFinal: Crypt funciona para qualquer tamanho de bloco, decrypt funciona apenas para blocos ímpares de 16 bytes cada.

--- cryptodevice.c
+++ cryptodevice.c
@@ -6,179 +6,211 @@
 #include <asm/uaccess.h>
 #include <linux/crypto.h>
 #include <linux/scatterlist.h>
-#include <linux/random.h>
-#include <crypto/skcipher.h>
+#include <crypto/hash.h>
 #define  DEVICE_NAME "cryptodevice"
 #define  CLASS_NAME  "cryptodevice"
 
 MODULE_LICENSE("GPL");
 
+#define SIZE_OF_DATA 16
+
 static int    majorNumber;
-static char   data[32];
-static short  size_of_data;
+static char   data[SIZE_OF_DATA * 4];
+static short  size_of_data = 0;
 static struct class*  cryptoClass  = NULL;
 static struct device* cryptoDevice = NULL;
-static char   operation;
+static char   operation = 0;
 static char   *key;
+static u8     criptografado[SIZE_OF_DATA / 2], descriptografado[SIZE_OF_DATA], criptografadoFinal[1000], descriptografadoFinal[1000];
+static u8     hashOutput[SIZE_OF_DATA];
 
+static ssize_t dev_read(struct file *, char *, size_t, loff_t *);
 static ssize_t dev_write(struct file *, const char *, size_t, loff_t *);
+
+static struct sdesc *init_sdesc(struct crypto_shash *alg);
+static int calc_hash(struct crypto_shash *alg, const unsigned char *data, unsigned int datalen, unsigned char *out);
 
 module_param(key, charp, 0000);
 
 static struct file_operations fops =
 {
-	.write = dev_write,
+   .read = dev_read,
+   .write = dev_write,
 };
 
-struct tcrypt_result {
-	struct completion completion;
-	int err;
+struct sdesc {
+    struct shash_desc shash;
+    char ctx[];
 };
-
-struct skcipher_def {
-	struct scatterlist sg;
-	struct crypto_skcipher *tfm;
-	struct skcipher_request *req;
-	struct tcrypt_result result;
-};
-
-static void test_skcipher_cb(struct crypto_async_request *req, int error);
-static void test_skcipher_encdec(struct skcipher_def *sk, int enc);
 
 static int __init cryptodevice_init(void){
-	printk(KERN_INFO "cryptodevice: inicializado com sucesso!\n");
-	printk(KERN_INFO "cryptomodule: key secreta é %s\n", key);
+   printk(KERN_INFO "cryptodevice: inicializado com sucesso!\n");
+   printk(KERN_INFO "cryptomodule: key secreta é %s\n", key);
 
-	majorNumber = register_chrdev(0, DEVICE_NAME, &fops);
-	if (majorNumber<0){
-		printk(KERN_ALERT "cryptodevice: falha ao criar o major number\n");
-		return majorNumber;
-	}
-	printk(KERN_INFO "cryptodevice: major number criado corretamente: %d\n", majorNumber);
+   majorNumber = register_chrdev(0, DEVICE_NAME, &fops);
+   if (majorNumber<0){
+      printk(KERN_ALERT "cryptodevice: falha ao criar o major number\n");
+      return majorNumber;
+   }
+   printk(KERN_INFO "cryptodevice: major number criado corretamente: %d\n", majorNumber);
 
-	cryptoClass = class_create(THIS_MODULE, CLASS_NAME);
-	if (IS_ERR(cryptoClass)){
-		unregister_chrdev(majorNumber, DEVICE_NAME);
-		printk(KERN_ALERT "cryptodevice: falha ao criar a classe do device\n");
-		return PTR_ERR(cryptoClass);
-	}
-	printk(KERN_INFO "cryptodevice: classe do device criada com sucesso!\n");
+   cryptoClass = class_create(THIS_MODULE, CLASS_NAME);
+   if (IS_ERR(cryptoClass)){
+      unregister_chrdev(majorNumber, DEVICE_NAME);
+      printk(KERN_ALERT "cryptodevice: falha ao criar a classe do device\n");
+      return PTR_ERR(cryptoClass);
+   }
+   printk(KERN_INFO "cryptodevice: classe do device criada com sucesso!\n");
 
-	cryptoDevice = device_create(cryptoClass, NULL, MKDEV(majorNumber, 0), NULL, DEVICE_NAME);
-	if (IS_ERR(cryptoDevice)){
-		class_destroy(cryptoClass);
-		unregister_chrdev(majorNumber, DEVICE_NAME);
-		printk(KERN_ALERT "cryptodevice: falha ao criar o device\n");
-		return PTR_ERR(cryptoDevice);
-	}
-	printk(KERN_INFO "cryptodevice: device criado com sucesso!\n");
-	return 0;
+   cryptoDevice = device_create(cryptoClass, NULL, MKDEV(majorNumber, 0), NULL, DEVICE_NAME);
+   if (IS_ERR(cryptoDevice)){
+      class_destroy(cryptoClass);
+      unregister_chrdev(majorNumber, DEVICE_NAME);
+      printk(KERN_ALERT "cryptodevice: falha ao criar o device\n");
+      return PTR_ERR(cryptoDevice);
+   }
+   printk(KERN_INFO "cryptodevice: device criado com sucesso!\n");
+   return 0;
 }
 
 static void __exit cryptodevice_exit(void){
-	device_destroy(cryptoClass, MKDEV(majorNumber, 0));
-	class_unregister(cryptoClass);
-	class_destroy(cryptoClass);
-	unregister_chrdev(majorNumber, DEVICE_NAME);
-	printk(KERN_INFO "cryptodevice: finalizado com sucesso!\n");
+   device_destroy(cryptoClass, MKDEV(majorNumber, 0));
+   class_unregister(cryptoClass);
+   class_destroy(cryptoClass);
+   unregister_chrdev(majorNumber, DEVICE_NAME);
+   printk(KERN_INFO "cryptodevice: finalizado com sucesso!\n");
 }
 
-static void test_skcipher_cb(struct crypto_async_request *req, int error)
-{
-	struct tcrypt_result *result = req->data;
+static ssize_t dev_read(struct file *filep, char *buffer, size_t len, loff_t *offset){
+   char buf[256];
 
-	if (error == -EINPROGRESS)
-		return;
-	result->err = error;
-	complete(&result->completion);
-	printk(KERN_INFO "Encryption finished successfully\n");
-}
+   buf[0] = operation;
 
-static void test_skcipher_encdec(struct skcipher_def *sk, int enc)
-{
-	int rc = 0;
+   if(operation == 'c') {
+      sprintf(&(buf[1]), "%s", criptografado);
+      copy_to_user(buffer, buf, sizeof(buf));
+   }
+   else if(operation == 'd') {
+      sprintf(&(buf[1]), "%s", descriptografado);
+      copy_to_user(buffer, buf, sizeof(buf));
+   }
+   else if(operation == 'h') {
+      sprintf(&(buf[1]), "%s", hashOutput);
+      copy_to_user(buffer, buf, sizeof(buf));
+   }
 
-	if (enc == 1)
-		rc = crypto_skcipher_encrypt(sk->req);
-	else
-		rc = crypto_skcipher_decrypt(sk->req);
-
-	init_completion(&sk->result.completion);
+   return 0;
 }
 
 static ssize_t dev_write(struct file *filep, const char *buffer, size_t len, loff_t *offset){
-	struct skcipher_def sk;
-	struct crypto_skcipher *skcipher = NULL;
-	struct skcipher_request *req = NULL;
-	char *scratchpad = NULL;
-	char *ivdata = NULL;
-	unsigned char key[32];// = "AAAABBBBAAAABBBBAAAABBBBAAAABBBB";
-	unsigned char *buf = NULL;
-	struct scatterlist resultado;
+	struct crypto_cipher *cryptoCipher = NULL;
+   struct crypto_shash *cryptoSHash = NULL;
+   int i, j, k = 0;
+   char aux[SIZE_OF_DATA], aux2[SIZE_OF_DATA];
 
-	int i;
+	cryptoCipher = crypto_alloc_cipher("aes", 0, 0);
 
-	skcipher = crypto_alloc_skcipher("cbc-aes-aesni", 0, 0);
-	if (IS_ERR(skcipher)) {
-		printk(KERN_INFO "could not allocate skcipher handle\n");
-		return 1;
+	if (IS_ERR(crypto_cipher_tfm(cryptoCipher))) {
+		pr_info("cryptodevice: não foi possível alocar o handle para o cipher\n");
+		return PTR_ERR(crypto_cipher_tfm(cryptoCipher));
 	}
 
-	req = skcipher_request_alloc(skcipher, GFP_KERNEL);
-	if (!req) {
-		printk(KERN_INFO "could not allocate skcipher request\n");
-		return 1;
+   cryptoSHash = crypto_alloc_shash("sha1", 0, 0);
+
+   if (crypto_cipher_setkey(cryptoCipher, key, 32 * sizeof(u8)) != 0) {
+      pr_info("cryptodevice: não foi possível definir a key\n");
+      return 1;
+   }
+   
+   //sprintf(data, "%s", &(buffer[2]));
+   strcpy(data, "AAAABBBBAAAABBBBCCCCDDDDCCCCDDDDEEEEFFFFEEEEFFFFGGGGHHHHGGGGHHHH");//14cef44fa783d5188ef4cf29ea6f6e05b5010067db421d4f077962ddc87ea65d
+
+   //operation = buffer[0];
+
+   printk(KERN_INFO "cryptodevice: os dados recebidos são %s\n", data);
+
+      for(j = 0; j < (strlen(data)/SIZE_OF_DATA) + 1; j++) {
+         strncpy(aux, (j * SIZE_OF_DATA) + &data[0], SIZE_OF_DATA);
+         //aux[SIZE_OF_DATA - 1] = '\0';
+         printk("teste: "); 
+
+	 for(i = 0; i < SIZE_OF_DATA; i++) {
+ 	    printk("%c", aux[i]); 
+	 } //printa hexa estranho no final se fizer com %s
+	 printk("\n"); 	 
+
+         size_of_data = strlen(data);
+	 
+         crypto_cipher_encrypt_one(cryptoCipher, criptografado, aux);
+
+         for(i = 0; i < SIZE_OF_DATA; i++) {
+            printk(KERN_CONT "%02x", criptografado[i]);
+	    criptografadoFinal[k] = criptografado[i];
+	    k++;
+         }
+	 printk("\n");
+      }
+
+      criptografadoFinal[k] = '\0';
+
+      for(i = 0; i < (SIZE_OF_DATA * 4); i++) {
+         printk(KERN_CONT "%02x", criptografadoFinal[i]);
+      }
+      printk("\n");
+
+	k = 0;	
+
+	for(j = 0; j < (strlen(data)/SIZE_OF_DATA) + 1; j++) {
+		strncpy(aux2, (j * SIZE_OF_DATA) + &criptografadoFinal[0], SIZE_OF_DATA);
+		//strncpy(aux, &criptografadoFinal[16], SIZE_OF_DATA);
+	   	crypto_cipher_decrypt_one(cryptoCipher, descriptografado, aux2);
+
+		for(i = 0; i < SIZE_OF_DATA; i++) {
+			printk(KERN_CONT "descriptografado: %c", descriptografado[i]);
+			descriptografadoFinal[k] = descriptografado[i];
+			k++;
+		}
+		printk("\n");
+
+		descriptografado[i] = '\0';
+
+	   	printk(KERN_INFO "cryptodevice: o resultado: %s\n", descriptografado);
 	}
 
-	skcipher_request_set_callback(req, CRYPTO_TFM_REQ_MAY_BACKLOG, test_skcipher_cb, &sk.result);
+	descriptografadoFinal[k] = '\0';
 
-	//TEM QUE VERIFICAR SE A PESSOA EH SACANA!!!!!!!!!! URGENTE
-	
-	/* AES 256 with random key */
-	get_random_bytes(&key, 32);
-	if (crypto_skcipher_setkey(skcipher, key, 32)) {
-		pr_info("key could not be set\n");
-		return 1;
+	for(i = 0; i < (SIZE_OF_DATA * 4); i++) {
+		printk(KERN_CONT "%c", descriptografadoFinal[i]);
 	}
-	
-	/* IV will be random */
-	ivdata = kmalloc(16, GFP_KERNEL);
-	if (!ivdata) {
-		pr_info("could not allocate ivdata\n");
-		return 1;
-	}
-	get_random_bytes(ivdata, 16);
+	printk("\n");
+   
+   crypto_free_cipher(cryptoCipher);
+   crypto_free_shash(cryptoSHash);
+   return len;
+}
 
-	/* Input data will be random */
-	scratchpad = kmalloc(16, GFP_KERNEL);
-	if (!scratchpad) {
-		pr_info("could not allocate scratchpad\n");
-		return 1;
-	}
-	get_random_bytes(scratchpad, 16);
+static struct sdesc *init_sdesc(struct crypto_shash *alg) {
+    struct sdesc *sdesc;
+    int size;
 
-	//scratchpad = "OITUDOBEMCOMVOCE";
+    size = sizeof(struct shash_desc) + crypto_shash_descsize(alg);
+    sdesc = kmalloc(size, GFP_KERNEL);
+    
+    sdesc->shash.tfm = alg;
+    sdesc->shash.flags = 0x0;
 
-	sk.tfm = skcipher;
-	sk.req = req;
+    return sdesc;
+}
 
-	sg_init_one(&sk.sg, scratchpad, 16);
-	skcipher_request_set_crypt(req, &sk.sg, &sk.sg, 16, ivdata);
-	init_completion(&sk.result.completion);
+static int calc_hash(struct crypto_shash *alg, const unsigned char *data, unsigned int datalen, unsigned char *out) {
+    struct sdesc *sdesc;
+    int ret;
 
-	test_skcipher_encdec(&sk, 1);
+    sdesc = init_sdesc(alg);
 
-	buf = kmalloc(16, GFP_KERNEL);
-	//sg_copy_to_buffer(&resultado, 1, &buf, 16);
-
-	printk(KERN_INFO "Encryption triggered successfully");
-	// for(i = 0; i < strlen(buf); i++) {
- //    	printk(KERN_CONT "%02x", buf[i]);
-	// }
-
-	//crypto_free_skcipher(skcipher);
-
-	return 1;
+    ret = crypto_shash_digest(&sdesc->shash, data, datalen, out);
+    kfree(sdesc);
+    return ret;
 }
 
 module_init(cryptodevice_init);
--- programa.c
+++ programa.c
@@ -5,33 +5,58 @@
 #include<string.h>
 #include<unistd.h>
 
-#define BUFFER_LENGTH 256               ///< The buffer length (crude but fine)
-static char receive[BUFFER_LENGTH];     ///< The receive buffer from the LKM
+#define BUFFER_LENGTH 17
+static char receive[BUFFER_LENGTH];
 
-int main(){
-   int ret, fd;
-   char operacaoDados[BUFFER_LENGTH];
-   fd = open("/dev/cryptodevice", O_RDWR);             // Open the device with read/write access
+//MENSAGEM TESTE: "OITUDOBEMCOMVOCE"
+
+int main() {
+   int ret, fd, i;
+   char operacaoDados[18];
+
+   fd = open("/dev/cryptodevice", O_RDWR);
+
    if (fd < 0){
       perror("Erro ao abrir o device!");
       return errno;
    }
-   printf("Entre com uma operação de criptografia (operacao dados):\n");
-   scanf("%[^\n]%*c", operacaoDados);                // Read in a string (with spaces)
-   ret = write(fd, operacaoDados, strlen(operacaoDados)); // Send the string to the LKM
+
+   printf("Entre com uma operação de criptografia (operacao dados)!\n");
+   scanf("%[^\n]%*c", operacaoDados);
+
+   ret = write(fd, operacaoDados, 18);
    if (ret < 0){
       perror("Erro ao escrever no device!");
       return errno;
    }
 
-   printf("Press ENTER to read back from the device...\n");
+   printf("Pressione ENTER para finalizar.\n");
    getchar();
 
-   ret = read(fd, receive, BUFFER_LENGTH);        // Read the response from the LKM
+   ret = read(fd, receive, BUFFER_LENGTH);
+   //Enviar na pos receive[1] o tamanho da msg
+   if(receive[0] == 'c') {
+      for(i = 1; i < BUFFER_LENGTH; i++) {
+         printf("%02X", (unsigned int)(receive[i] & 0xFF));
+      }
+   }
+   else if(receive[0] == 'd') {
+      for(i = 1; i < BUFFER_LENGTH; i++) {
+         printf("%c", receive[i]);
+      }
+   }
+   else if(receive[0] == 'h') {
+      for(i = 1; i < BUFFER_LENGTH; i++) {
+         printf("%02X", (unsigned int)(receive[i] & 0xFF));
+      }
+   }
+
    if (ret < 0){
-      perror("Failed to read the message from the device.");
+      perror("Erro ao receber do device!");
       return errno;
    }
-   printf("The received message is: [%s]\n", receive);
+
+   printf("\n");
+
    return 0;
 }
